58cc8f174292b39d21ae4f96f9d0b1b21a001488
cristiane <cris_csr@yahoo.com.br>
2019-03-20 22:49:03 -0300

    primeiro commit

---
+++ src/estrutural2/C.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estrutural2;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class C {
+
+    int codigoC;
+    String nomeC;
+
+    public int getCodigoC() {
+        return this.codigoC;
+    }
+
+    public void setCodigoC(int codigoC) {
+        this.codigoC = codigoC;
+    }
+}
